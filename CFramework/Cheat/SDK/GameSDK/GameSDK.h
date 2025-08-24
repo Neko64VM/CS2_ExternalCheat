3df77fdf1e98ac2ae6f9f59ee94affe6b9e6e9fc
Neko64VM <neko64v@gmail.com>
2025-08-24 23:28:11 +0900

    Update offset

--- CFramework/Cheat/SDK/GameSDK/GameSDK.h
+++ CFramework/Cheat/SDK/GameSDK/GameSDK.h
@@ -1,16 +1,11 @@
 #pragma once
-#include "a2x/buttons.hpp"
 #include "a2x/client_dll.hpp"
-#include "a2x/offsets.hpp"
 #include "../../../Framework/Config/Config.h"
 using namespace cs2_dumper::schemas::client_dll;
 
 namespace offset
 {
-	// Address
-	constexpr auto dwViewAngles = cs2_dumper::offsets::client_dll::dwViewAngles;
-
-	// Offsets
+	// Player
 	constexpr auto m_hPlayerPawn = CCSPlayerController::m_hPlayerPawn;
 	constexpr auto m_sSanitizedPlayerName = CCSPlayerController::m_sSanitizedPlayerName;
 	constexpr auto m_fFlags = C_BaseEntity::m_fFlags;
@@ -19,9 +14,9 @@ namespace offset
 	constexpr auto m_iTeamNum = C_BaseEntity::m_iTeamNum;
 	constexpr auto m_pCollision = C_BaseEntity::m_pCollision;
 	constexpr auto m_pGameSceneNode = C_BaseEntity::m_pGameSceneNode;
-	constexpr auto m_vecLastClipCameraPos = C_CSPlayerPawnBase::m_vecLastClipCameraPos;
-	constexpr auto m_angEyeAngles = C_CSPlayerPawnBase::m_angEyeAngles;
-	constexpr auto m_pClippingWeapon = C_CSPlayerPawnBase::m_pClippingWeapon;
+	constexpr auto m_vecLastClipCameraPos = C_CSPlayerPawn::m_vecLastClipCameraPos;
+	constexpr auto m_angEyeAngles = C_CSPlayerPawn::m_angEyeAngles;
+	constexpr auto m_pClippingWeapon = C_CSPlayerPawn::m_pClippingWeapon;
 	constexpr auto m_vOldOrigin = C_BasePlayerPawn::m_vOldOrigin;
 	constexpr auto m_modelState = CSkeletonInstance::m_modelState;
 	constexpr auto m_bPawnIsAlive = CCSPlayerController::m_bPawnIsAlive;
